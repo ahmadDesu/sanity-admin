d618476d7024e05f6247be8dfb70481f80c05a9b
Ahmad Adyla <madrista982@gmail.com>
2023-07-26 17:59:51 +0700

    menambahkan rich text pada pet content

--- schemas/pet.ts
+++ schemas/pet.ts
@@ -1,15 +1,20 @@
-import { defineField, defineType } from 'sanity'
+import {defineField, defineType} from 'sanity'
 
 export default defineType({
-    name: 'pet',
-    type: 'document',
-    title: 'Pet',
-    fields: [
-      defineField({
-        name: 'name',
-        type: 'string',
-        title: 'Name',
-      }),
-    ],
-  })
-  
+  name: 'pet',
+  type: 'document',
+  title: 'Pet',
+  fields: [
+    defineField({
+      name: 'name',
+      type: 'string',
+      title: 'Name',
+    }),
+    defineField({
+      name: 'content',
+      title: 'Content',
+      type: 'array',
+      of: [{type: 'block'}, {type: 'image'}],
+    }),
+  ],
+})
